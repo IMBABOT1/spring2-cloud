083e8bf5f70aa338f768f62fb3e306868f1f404f
unknown <zlovred12@gmail.com>
2022-12-03 21:29:57 +0300

    temp

---
+++ product-service/src/main/java/ru/geekbrains/product/Product.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.product;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Integer price;
+}
